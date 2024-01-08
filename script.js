a6c57b97f96a47f2a6b2bb4476d561ae60441207
Chandan Gupta <112531619+Chandujixyz@users.noreply.github.com>
2024-01-08 23:14:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+ var first=prompt("Take input for the first variable;");
+  var mul=parseInt(first)*2;
+  var second=prompt("Take another input for the second variable:");
+  var result=mul+parseInt(second);
+  alert("The final result is: " + result);
